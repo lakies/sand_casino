a5ab37542c4d5765d495325c6866128eeb32520e
Joosep Kaimre <joosepka@gmail.com>
2019-03-18 22:45:46 +0200

    muudatus

--- src/main/java/Games/CoinFlip.java
+++ src/main/java/Games/CoinFlip.java
@@ -6,8 +6,10 @@ import Server.ClientData;
 public class CoinFlip {
     public static void main(String[] args) {
 
+
     }
     public void coinflip(ClientData client){
+
         double i = Math.random();
         if (i < 0.5){
             //heads
