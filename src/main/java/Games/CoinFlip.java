4256b2b99104eca83acac9fc7a6bfb72ad9ba0af
Joosep Kaimre <joosepka@gmail.com>
2019-03-18 13:42:15 +0200

    coinflipi alge

--- src/main/java/Games/CoinFlip.java
+++ src/main/java/Games/CoinFlip.java
@@ -1,5 +1,18 @@
 package Games;
 
-public class CoinFlip {
+import Server.ClientData;
 
+
+public class CoinFlip {
+    public static void main(String[] args) {
+
+    }
+    public void coinflip(ClientData client){
+        double i = Math.random();
+        if (i < 0.5){
+            //heads
+        } else {
+            //tails
+        }
+    }
 }
