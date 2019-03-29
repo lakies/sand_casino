8f379903db321e157ece6677b2cb2358d6d5d618
Joosep Kaimre <joosepka@gmail.com>
2019-03-29 21:37:19 +0200

    Enhanced the lottery

--- src/main/java/Games/Lottery.java
+++ src/main/java/Games/Lottery.java
@@ -1,6 +1,18 @@
 package Games;
 
+import Server.ClientData;
+
 public class Lottery extends GameInstance {
+
+    public Lottery(){
+        super(50000, 1);
+        
+    }
+    @Override
+    public boolean enoughFunds(ClientData client) {
+        return false;
+    }
+
     @Override
     public void runGameLogic() {
 
